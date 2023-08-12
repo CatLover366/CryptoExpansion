6c9899182580181a3a3fe4d5c69a0b6f3a2fada7
Zomuro <mrscratchtwo@gmail.com>
2023-08-12 00:44:00 -0400

    added c# for ability code
    + added a generalized wallraise abilitycomp
    + added source folder
    + added gitignore and loadfolder

---
+++ Source/CryptoExpansion/CompAssociated.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace CryptoExpansion
+{
+    public class CompAssociated : ThingComp
+    {
+        public override void PostExposeData()
+        {
+            Scribe_References.Look(ref pawn, "pawn");
+        }
+
+        public Pawn pawn;
+
+    }
+}
